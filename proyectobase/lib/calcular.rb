b19bdb6f8c67519e67b6fccbcc9a67feab6dd632
Harold Muyba <hmuyba@gmail.com>
2021-05-25 10:11:17 -0400

    segunda prueba tdd

--- proyectobase/lib/calcular.rb
+++ proyectobase/lib/calcular.rb
@@ -4,4 +4,11 @@ def calcularCambio(saldo)
         array=["0.5","0.5"]
         return array
     end
+
+    if(saldo==5)
+        array=["2","2","1"]
+        return array
+
+    end
+
 end
--- proyectobase/spec/cambiarmonedas_spec.rb
+++ proyectobase/spec/cambiarmonedas_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe"cambiar monedas" do
     it "deberia devolver 2 monedas de 0,5 para 1 boliviano" do
         expect(calcularCambio(1)).to eq ["0.5","0.5"]
     end
+
+    it "deberia devolver 2 monedas de 2 bs y una de 1 bs para 5 bolivianos" do
+        expect(calcularCambio(5)).to eq ["2","2","1"]
+    end
+
+    
 end
